218f7bc90c58c687a41313c3537cd99c09d341c1
benoit <benoit@assetik.com>
2022-04-27 15:39:10 +0400

    feat: setup truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -4,12 +4,23 @@ var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble s
 module.exports = {
   networks: {
     development: {
-      provider: function() {
+      provider: function () {
         return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
       },
       network_id: '*',
       gas: 9999999
-    }
+    },
+    ganache_cli: {
+      // provider: () => new HDWalletProvider(mnemonic, `https://127.0.0.1:8545/`),
+      host: "localhost",
+      port: 8545,
+      network_id: '*',
+      gas: 9999999,
+      networkCheckTimeout: 999999,
+      websockets: true, // Enable EventEmitter interface for web3 (default: false)
+      networkCheckTimeoutnetworkCheckTimeout: 10000,
+      timeoutBlocks: 200
+    },
   },
   compilers: {
     solc: {
