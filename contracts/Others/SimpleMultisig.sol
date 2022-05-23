f32eee2bbf4d277313a4dad787381028b51e79d9
benoit <benoit@assetik.com>
2022-05-23 09:37:25 +0400

    feat: add others\SimpleMultiSig.sol (not related to the project)

---
+++ contracts/Others/SimpleMultisig.sol
@@ -0,0 +1,63 @@
+pragma solidity 0.4.14;
+
+contract SimpleMultiSig {
+  uint256 public nonce; // (only) mutable state
+  uint256 public threshold; // immutable state
+  mapping(address => bool) isOwner; // immutable state
+  address[] public ownersArr; // immutable state
+
+  function SimpleMultiSig(uint256 threshold_, address[] owners_) {
+    if (owners_.length > 10 || threshold_ > owners_.length || threshold_ == 0) {
+      throw;
+    }
+
+    for (uint256 i = 0; i < owners_.length; i++) {
+      isOwner[owners_[i]] = true;
+    }
+    ownersArr = owners_;
+    threshold = threshold_;
+  }
+
+  // Note that address recovered from signatures must be strictly increasing
+  function execute(
+    uint8[] sigV,
+    bytes32[] sigR,
+    bytes32[] sigS,
+    address destination,
+    uint256 value,
+    bytes data
+  ) {
+    if (sigR.length != threshold) {
+      throw;
+    }
+    if (sigR.length != sigS.length || sigR.length != sigV.length) {
+      throw;
+    }
+
+    // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
+    bytes32 txHash = sha3(
+      bytes1(0x19),
+      bytes1(0),
+      this,
+      destination,
+      value,
+      data,
+      nonce
+    );
+
+    address lastAdd = address(0); // cannot have address(0) as an owner
+    for (uint256 i = 0; i < threshold; i++) {
+      address recovered = ecrecover(txHash, sigV[i], sigR[i], sigS[i]);
+      if (recovered <= lastAdd || !isOwner[recovered]) throw;
+      lastAdd = recovered;
+    }
+
+    // If we make it here all signatures are accounted for
+    nonce = nonce + 1;
+    if (!destination.call.value(value)(data)) {
+      throw;
+    }
+  }
+
+  function() payable {}
+}
